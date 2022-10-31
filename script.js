df658547876538026a2262912ea2ac6928107f42
pharaohmak <61184060+pharaohmak@users.noreply.github.com>
2022-10-31 06:06:19 -0600

    edit portfolio

--- script.js
+++ script.js
@@ -8,3 +8,31 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+function navAll() {
+    document.querySelector("#allTab").style.display="block"
+    document.querySelector("#websitesTab").style.display="none"
+    document.querySelector("#appsTab").style.display="none"
+    document.querySelector("#frameworksTab").style.display="none"
+}
+
+function navWebsites() {
+    document.querySelector("#allTab").style.display = "none";
+    document.querySelector("#websitesTab").style.display = "block"
+    document.querySelector("#appsTab").style.display="none"
+    document.querySelector("#frameworksTab").style.display="none"
+}
+
+function navApps() {
+    document.querySelector("#appsTab").style.display="block"
+    document.querySelector("#allTab").style.display="none"
+    document.querySelector("#frameworksTab").style.display="none"
+    document.querySelector("#websitesTab").style.display="none"
+}
+
+function navFrameworks() {
+    document.querySelector("#frameworksTab").style.display="block"
+    document.querySelector("#allTab").style.display="none"
+    document.querySelector("#websitesTab").style.display="none"
+    document.querySelector("#appsTab").style.display="none"
+}
