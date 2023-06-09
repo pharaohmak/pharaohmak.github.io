673fc42f61d7776885a506e478a1bff940b03006
pharaohmak <61184060+pharaohmak@users.noreply.github.com>
2023-06-08 21:18:01 -0400

    refactored

--- README.md
+++ README.md
@@ -6,25 +6,30 @@
 [![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/)
 
 ## 🚀 About Me
-I'm a full stack developer who works with small-business owners 
-to create technology soloutions that help automate daily tasks.
 
-## 🛠 Skills
+I'm a full stack developer who works with small-business owners to create technology soloutions that help automate daily tasks.
 
-#### Programming Languages
+## 🛠 Technical Skills
+
+### Programming Languages
+
 Proficient in HTML, CSS, and Javascript
+
 - Python
 - PHP
 - Java
 - C#
 
 #### CSS Preprocessors
+
 - Bootstrap
 - Typescript
 - Sass
 
 #### Frameworks
+
 React and React Native Specialist
+
 - Swift Ui
 - Angular
 - Django
--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function navAll() {
     document.querySelector("#componentsTab").style.display = "none"
     document.querySelector("#programsTab").style.display = "none"
     document.querySelector("#allTab").style.display = "block"
+
+    document.querySelector('navAllBtn').addClass('active')
+    document.querySelector('navWebsitesBtn').removeClass('active')
+    document.querySelector('navProgramsBtn').removeClass('active')
+    document.querySelector('navComponentsBtn').removeClass('active')
 }
 
 function navWebsites() {
@@ -37,3 +42,8 @@ function navPrograms() {
     document.querySelector("#websitesTab").style.display = "none"
     document.querySelector("#programsTab").style.display = "block"
 }
+
+function ready() {
+    document.querySelector('navAllBtn')
+}
+
