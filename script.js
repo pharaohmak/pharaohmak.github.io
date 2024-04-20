a79e8d1bea764eae8d0f9ee5e577b8a7aea6b533
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-04-20 17:45:51 -0600

    refactored

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 // Script to open and close sidebar
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
 }
 
 function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
 }
 
 // Script to navigate portfolio tabs
 function navAll() {
-    document.querySelector("#websitesTab").style.display = "none"
-    document.querySelector("#componentsTab").style.display = "none"
-    document.querySelector("#programsTab").style.display = "none"
-    document.querySelector("#allTab").style.display = "block"
+  document.querySelector("#capstone-tab").style.display = "block";
+  document.querySelector("#website-tab").style.display = "none";
+  document.querySelector("#app-tab").style.display = "none";
+  document.querySelector("#programs-tab").style.display = "none";
+
+  document.getElementById("capstone-tab").style.backgroundColor = "#000";
+  document.getElementById("website-tab").style.backgroundColor = "#fff";
+  document.getElementById("app-tab").style.backgroundColor = "#fff";
+  document.getElementById("programs-tab").style.backgroundColor = "#fff";
 }
 
 function navWebsites() {
-    document.querySelector("#allTab").style.display = "none";
-    document.querySelector("#componentsTab").style.display = "none"
-    document.querySelector("#programsTab").style.display = "none"
-    document.querySelector("#websitesTab").style.display = "block"
+  document.querySelector("#capstone-tab").style.display = "none";
+  document.querySelector("#website-tab").style.display = "block";
+  document.querySelector("#app-tab").style.display = "none";
+  document.querySelector("#programs-tab").style.display = "none";
+
+  document.getElementById("capstone-tab").style.backgroundColor = "#fff";
+  document.getElementById("website-tab").style.backgroundColor = "#000";
+  document.getElementById("app-tab").style.backgroundColor = "#fff";
+  document.getElementById("programs-tab").style.backgroundColor = "#fff";
 }
 
 function navComponents() {
-    document.querySelector("#websitesTab").style.display = "none"
-    document.querySelector("#allTab").style.display = "none"
-    document.querySelector("#programsTab").style.display = "none"
-    document.querySelector("#componentsTab").style.display = "block"
+  document.querySelector("#capstone-tab").style.display = "none";
+  document.querySelector("#website-tab").style.display = "none";
+  document.querySelector("#app-tab").style.display = "block";
+  document.querySelector("#programs-tab").style.display = "none";
+
+  document.getElementById("capstone-tab").style.backgroundColor = "#fff";
+  document.getElementById("website-tab").style.backgroundColor = "#fff";
+  document.getElementById("app-tab").style.backgroundColor = "#000";
+  document.getElementById("programs-tab").style.backgroundColor = "#fff";
 }
 
 function navPrograms() {
-    document.querySelector("#allTab").style.display = "none"
-    document.querySelector("#componentsTab").style.display = "none"
-    document.querySelector("#websitesTab").style.display = "none"
-    document.querySelector("#programsTab").style.display = "block"
+  document.querySelector("#capstone-tab").style.display = "none";
+  document.querySelector("#website-tab").style.display = "none";
+  document.querySelector("#app-tab").style.display = "none";
+  document.querySelector("#programs-tab").style.display = "block";
+
+  document.getElementById("capstone-tab").style.backgroundColor = "#fff";
+  document.getElementById("website-tab").style.backgroundColor = "#fff";
+  document.getElementById("app-tab").style.backgroundColor = "#fff";
+  document.getElementById("programs-tab").style.backgroundColor = "#000";
 }
-
-
